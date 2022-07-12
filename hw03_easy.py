d38b3cc8f32a57f9bdf2c8d2d72d05f6fa832d64
LemanRus <wlad163163@gmail.com>
2022-07-12 10:33:48 +0400

    Hw03 Task 2 Easy

--- hw03_easy.py
+++ hw03_easy.py
@@ -7,3 +7,16 @@ fruits = ["banana", "apple", "pineapple", "melon", "watermelon"]
 
 for fruit in fruits:
     print("{:>3}. {:>12}".format(fruits.index(fruit) + 1, fruit))
+
+# Задача-2:
+# Даны два произвольные списка.
+# Удалите из первого списка элементы, присутствующие во втором списке.
+
+scientists = ["Alex", "Bob", "Mike", "John", "Tom", "Jim"]
+chemists = ["Amber", "Mike", "Rob", "Jim"]
+
+for name in scientists:
+    if name in chemists:
+        scientists.remove(name)
+
+print(scientists)
