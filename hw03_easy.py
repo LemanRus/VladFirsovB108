482d9d0230b738f0019dd1af8eeb3b5febe182ac
LemanRus <wlad163163@gmail.com>
2022-07-12 11:27:18 +0400

    Hw03 Task 3 Easy

--- hw03_easy.py
+++ hw03_easy.py
@@ -20,3 +20,21 @@ for name in scientists:
         scientists.remove(name)
 
 print(scientists)
+
+# Задача-3:
+# Дан произвольный список из целых чисел.
+# Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
+# если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+
+my_list = [2, 23, 4, 54, 6, 786, 45, 32, 12, 11, 73]
+new_list = []
+
+for i in my_list:
+    if i % 2 == 0:
+        new_list.append(i / 4)
+    else:
+        new_list.append(i * 2)
+print(new_list)
+
+# new_list = [x / 4 if x % 2 == 0 else x * 2 for x in my_list]
+# print(new_list)
