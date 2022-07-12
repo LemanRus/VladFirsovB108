4264df0db4f610a9a60e604e62aba1e580eef318
LemanRus <wlad163163@gmail.com>
2022-07-12 23:24:52 +0400

    Hw03 Task 4 Normal

--- hw03_normal.py
+++ hw03_normal.py
@@ -69,3 +69,22 @@ for i in range(quantity):
 # rand_list = [random.randint(-100, 100) for x in range(quantity)]
 
 print(rand_list)
+
+# Задача-4: Дан список, заполненный произвольными целыми числами.
+# Получите новый список, элементами которого будут:
+# а) неповторяющиеся элементы исходного списка:
+# например, lst = [1, 2, 4, 5, 6, 2, 5, 2], нужно получить lst2 = [1, 2, 4, 5, 6]
+# б) элементы исходного списка, которые не имеют повторений:
+# например, lst = [1 , 2, 4, 5, 6, 2, 5, 2], нужно получить lst2 = [1, 4, 6]
+
+int_list = [1, 2, 4, 5, 6, 2, 5, 2]
+
+unique_list = list(set(int_list))
+alone_list = []
+for x in int_list:
+    if int_list.count(x) == 1:
+        alone_list.append(x)
+# alone_list = [x for x in int_list if int_list.count(x) == 1]
+
+print(unique_list)
+print(alone_list)
