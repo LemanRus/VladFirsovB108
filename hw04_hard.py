48425d44154b39d7f8cde7d5414a947b37a58b0f
LemanRus <wlad163163@gmail.com>
2022-07-17 19:30:46 +0400

    Hw04 Task 1 Hard

--- hw04_hard.py
+++ hw04_hard.py
@@ -4,10 +4,61 @@
 # n x/y ,где n - целая часть, x - числитель, у - знаменатель.
 # Дроби могут быть отрицательные и не иметь целой части, или иметь только целую часть.
 # Примеры:
-# Ввод: 5/6 + 4/7 (всё выражение вводится целиком в виде строки)
+# Ввод: -4 5/6 + -5 4/7 (всё выражение вводится целиком в виде строки)
 # Вывод: 1 17/42  (результат обязательно упростить и выделить целую часть)
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
+
+
+import re
+
+expression = input("Введите выражние: ")
+
+fracs = re.split(r"\s[+-]\s", expression)  # Вычленим дроби
+action_sign = True if re.findall(r"\s[+]\s", expression) else False  # И поймём, что с ними делать
+
+frac1, frac2 = fracs
+
+
+def dissolve_frac(frac):  # Для избегания повторения кода
+    frac_int = 0 if not frac.split(" ")[0] else int(frac.split(" ")[0])
+    frac_frac = frac.split(" ")[1]
+    frac_frac_enumerator = int(frac_frac.split("/")[0])
+    frac_frac_denominator = int(frac_frac.split("/")[1])
+    frac_enumerator = frac_int * frac_frac_denominator + frac_frac_enumerator if frac_int >= 0 else \
+        frac_int * frac_frac_denominator - frac_frac_enumerator
+    return frac_enumerator, frac_frac_denominator  # Получаем неправильную дробь
+
+
+frac1_enumerator, frac1_denominator = dissolve_frac(frac1)
+frac2_enumerator, frac2_denominator = dissolve_frac(frac2)
+
+common_denominator = frac1_denominator * frac2_denominator  # Находим общий знаменатель самым надёжным образом
+
+frac1_enumerator_acted = frac1_enumerator * frac2_denominator  # Умножаем также и числитель
+frac2_enumerator_acted = frac2_enumerator * frac1_denominator
+
+common_enumerator = frac1_enumerator_acted + frac2_enumerator_acted if action_sign else \
+    frac1_enumerator_acted - frac2_enumerator_acted  # Производим собственно расчёт
+
+common_int = common_enumerator // common_denominator  # Выделяем целую часть
+common_frac_enumerator = common_enumerator % common_denominator  # Числитель дробной части
+
+if common_frac_enumerator != 0:  # Если дробная часть есть
+    if common_denominator % common_frac_enumerator == 0:
+        common_denominator = common_denominator // common_frac_enumerator
+        common_frac_enumerator = 1
+    elif common_denominator % frac1_denominator == 0 and common_frac_enumerator % frac1_denominator == 0:  # Некоторое упрощение
+        common_frac_enumerator = common_frac_enumerator // frac1_denominator
+        common_denominator = common_denominator // frac1_denominator
+    elif common_denominator % frac2_denominator == 0 and common_frac_enumerator % frac2_denominator == 0:
+        common_frac_enumerator = common_frac_enumerator // frac2_denominator
+        common_denominator = common_denominator // frac2_denominator
+
+if common_frac_enumerator:  # Дроби типа 6/9 выводятся в неупрощённом виде, поиск наименьшего общего делителя будет объёмным)
+    print("{} {}/{}".format(common_int, common_frac_enumerator, common_denominator))
+else:
+    print(str(common_int))
 
 
 # Задание-2:
