bb0ec2764b70cb5d619a366e5a5f0bcb06f595e1
LemanRus <wlad163163@gmail.com>
2022-07-19 16:50:15 +0400

    Hw05 Task 3 Normal in progress

--- hw05_normal.py
+++ hw05_normal.py
@@ -1,9 +1,11 @@
+import random, re
+
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
-
+import re
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -53,3 +55,11 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+
+num_list = [str(random.randint(0, 9)) for _ in range(0, 2500)]
+num_list[0] = str(random.randint(1, 9))
+
+num = "".join(num_list)
+nums = re.findall("\d\1+", num)
+print(nums)
