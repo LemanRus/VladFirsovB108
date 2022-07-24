6b6e60e4cb82de74db698fb27c9f79df235a3d6e
LemanRus <wlad163163@gmail.com>
2022-07-24 22:50:48 +0400

    Hw06 Task 1 Hard

--- hw06_hard.py
+++ hw06_hard.py
@@ -31,3 +31,57 @@
     Предусмотреть обработку исключений, например, если пытаются посмотреть все файлы не у папки, а у файла и.т.д.
 """
 
+
+import argparse
+import os
+import sys
+
+parser = argparse.ArgumentParser(description='Process commands')
+
+parser.add_argument('-touch', type=str, help='создаёт файл')
+parser.add_argument('-rm', type=str, help='удаляет указанный файл')
+parser.add_argument('-mkdir', type=str, help='создаёт папку')
+parser.add_argument('-ls', type=str, help='выдаёт информацию о каталоге')  # Порядок важен для использования нескольких команд в одной строке
+                                                                           # Как пофиксить порядок обработки, не придумал
+
+args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+
+def del_file(path):
+    try:
+        os.remove(os.path.join(path))
+    except FileNotFoundError:
+        print(f"Файл {path} не существует")
+
+
+def make_dir(path):
+    try:
+        os.mkdir(os.path.join(path))
+    except FileExistsError:
+        print(f"Директория {path} существует!")
+    else:
+        print(f"Папка {path} создана")
+
+
+def list_dir(path):
+    for item in os.listdir(os.path.join(path)):
+        print(item)
+
+
+def make_file(path):
+    os.makedirs(os.path.split(os.path.join(path))[0], exist_ok=True)
+    with open(os.path.join(path), "a"):  # Файл как бы есть, даже если уже был) Но пусть будет НЕ перезаписан
+        pass
+    print(f"Файл {path} создан")
+
+
+actions = {
+    "touch": make_file,
+    "rm": del_file,
+    "mkdir": make_dir,
+    "ls": list_dir,
+}
+
+for arg, parameter in vars(args).items():  # Наверняка есть более правильный и изящный способ,
+    if parameter:                          # но и этот работает))
+        actions[arg](parameter)
