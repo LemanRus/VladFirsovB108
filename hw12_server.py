8d84cc426376d0a3c3d0ff2419073ef2ffe07196
LemanRus <wlad163163@gmail.com>
2022-08-16 15:17:35 +0400

    Hw12 Example client-server

---
+++ hw12_server.py
@@ -0,0 +1,32 @@
+import socket
+import time
+
+host = "localhost"
+port = 9090
+
+clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+quit = False
+print("[SERVER STARTED]")
+
+while not quit:
+    try:
+        data, addr = s.recvfrom(1024)
+
+        if addr not in clients:
+            clients.append(addr)
+
+        itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+        print("[" + addr[0] + "]=[" + str(addr[1]) + "]=[" + itsatime + "]/", end="")
+        print(data.decode("utf-8"))
+        for client in clients:
+            if addr != client:
+                s.sendto(data, client)
+    except Exception as ex:
+        print(ex)
+        print("\n[SERVER STOPPED]")
+        quit = True
+
+s.close()
