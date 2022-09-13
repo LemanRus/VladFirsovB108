4212180c27acb9b9a2261b3d41419d3a56a70c9b
LemanRus <wlad163163@gmail.com>
2022-09-13 16:42:10 +0400

    Hw14 Made function join_chat in client

--- hw14_client.py
+++ hw14_client.py
@@ -5,10 +5,11 @@ import json
 import re
 
 
-class MyClient():
+class MyClient:
     def __init__(self):
         self.shutdown = False
         self.join = False
+        self.server_ok = False
 
         self.server = ("localhost", 9090)
 
@@ -29,41 +30,31 @@ class MyClient():
                     if response:
                         if response == 200:
                             print(rec_msg.get("message"))
+                            self.server_ok = True
                             return rec_msg.get("message")
-                        if response == 201:
-                            pass
-                        elif response == 202:
-                            pass
                         elif response == 404:
                             print("User", rec_msg.get("addresate"), "not found")
+                            self.server_ok = True
                             return "not found"
                         elif response == 503:
                             print("Server shutdown")
+                            self.server_ok = False
                             self.shutdown = True
                             return "Server shutdown"
+                        time.sleep(0.2)
 
-                    time.sleep(0.2)
             except Exception as ex:
+                print(ex)
                 return ex
 
     def send_msg(self):
         while not self.shutdown:
             if not self.join:
-                json_message = json.dumps({
-                    "action": "join_chat",
-                    "time": time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime()),
-                    "user": {
-                        "name": self.name,
-                        "status": "online"
-                    }
-                }).encode("utf-8")
-                self.s.sendto(json_message, self.server)
-                self.join = True
-                return json_message
+                self.join_chat()
             else:
                 try:
                     message_text = input("[YOU] :: ")
-                    if message_text != "":
+                    if message_text:
                         addresate = re.findall(r"^(\w+):", message_text)
                         msg_to_server = {
                             "action": "send_msg",
@@ -77,21 +68,41 @@ class MyClient():
                             msg_to_server["addresate"] = str(addresate[0])
                             msg_to_server["message"] = "From " + self.name + ": " + msg_to_server.get("message")
                         json_message = json.dumps(msg_to_server).encode("utf-8")
-                        self.s.sendto(json_message, self.server)
+                        sent = self.s.sendto(json_message, self.server)
+                        print(sent)
+                        if sent != 0:
+                            self.server_ok = True
                         return json_message
                     time.sleep(0.2)
                 except Exception as ex:
+                    print("HERESY!")
                     print(ex)
-                    json_message = json.dumps({
-                        "action": "leave_chat",
-                        "time": time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime()),
-                        "user": {
-                            "name": self.name,
-                            "status": "offline"
-                        }
-                    }).encode("utf-8")
-                    self.s.sendto(json_message, self.server)
+                    if self.server_ok:
+                        json_message = json.dumps({
+                            "action": "leave_chat",
+                            "time": time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime()),
+                            "user": {
+                                "name": self.name,
+                                "status": "offline"
+                            }
+                        }).encode("utf-8")
+                        self.s.sendto(json_message, self.server)
                     self.shutdown = True
+
+    def join_chat(self):
+        json_message = json.dumps({
+            "action": "join_chat",
+            "time": time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime()),
+            "user": {
+                "name": self.name,
+                "status": "online"
+            }
+        }).encode("utf-8")
+        sent = self.s.sendto(json_message, self.server)
+        if sent != 0:
+            self.server_ok = True
+        self.join = True
+        return json_message
 
 
 if __name__ == "__main__":
@@ -104,4 +115,5 @@ if __name__ == "__main__":
 
     sT.join()
     rT.join()
+
     my_client.s.close()
