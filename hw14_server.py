fc651818f4fa456ad450955594cbf60d1ebe8e6e
LemanRus <wlad163163@gmail.com>
2022-09-22 16:36:21 +0400

    Hw12-14 Server actions divided to separate methods

--- hw14_server.py
+++ hw14_server.py
@@ -28,6 +28,7 @@ class MyServer:
                 itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
 
                 print("[" + addr[0] + "]=[" + str(addr[1]) + "]=[" + itsatime + "] / ", end="")
+
                 action, addresate, user_name, message, send_data = self.disassemble_msg(rec_data)
                 self.assemble_answer(action, addresate, user_name, message, send_data, addr)
             except Exception as ex:
@@ -62,41 +63,52 @@ class MyServer:
 
     def assemble_answer(self, action, addresate, user_name, message, send_data, addr):
         if action == "join_chat":
-            print(user_name, "=> join chat")
-            send_data["response"] = 201
-            for client in self.clients:
-                if addr == client:
-                    self.client_names[user_name] = addr
-                    self.s.sendto(json.dumps(send_data).encode("utf-8"), client)
-                    print("sent answer")
+            self.client_joined(user_name, send_data, addr)
 
         elif action == "leave_chat":
             print(user_name, "<= left chat")
 
         elif addresate:
             if addresate in self.client_names.keys():
-                print(user_name, "to ->", addresate,
-                      "::", message)
-                for client in self.clients:
-                    if addr == client:
-                        send_data["response"] = 202
-                        self.s.sendto(json.dumps(send_data).encode("utf-8"), client)
-                send_data["response"] = 200
-                self.s.sendto(json.dumps(send_data).encode("utf-8"), self.client_names[addresate])
+                self.send_private_message(addresate, user_name, message, send_data, addr)
             else:
-                print("client", addresate, "not found")
-                send_data["response"] = 404
-                for client in self.clients:
-                    if addr == client:
-                        self.s.sendto(json.dumps(send_data).encode("utf-8"), client)
+                self.addresate_not_found()
         else:
-            print(user_name, "::", message)
-            for client in self.clients:
-                if addr == client:
-                    send_data["response"] = 202
-                else:
-                    send_data["response"] = 200
+            self.send_message()
+
+    def client_joined(self, user_name, send_data, addr):
+        print(user_name, "=> join chat")
+        send_data["response"] = 201
+        for client in self.clients:
+            if addr == client:
+                self.client_names[user_name] = addr
                 self.s.sendto(json.dumps(send_data).encode("utf-8"), client)
+
+    def send_private_message(self, addresate, user_name, message, send_data, addr):
+        print(user_name, "to ->", addresate, "::", message)
+        for client in self.clients:
+            if addr == client:
+                send_data["response"] = 202
+                self.s.sendto(json.dumps(send_data).encode("utf-8"), client)
+        send_data["response"] = 200
+        self.s.sendto(json.dumps(send_data).encode("utf-8"), self.client_names[addresate])
+
+    def addresate_not_found(self, addresate, send_data, addr):
+        print("client", addresate, "not found")
+        send_data["response"] = 404
+        for client in self.clients:
+            if addr == client:
+                self.s.sendto(json.dumps(send_data).encode("utf-8"), client)
+
+    def send_message(self, user_name, message, send_data, addr):
+        print(user_name, "::", message)
+        for client in self.clients:
+            if addr == client:
+                send_data["response"] = 202
+            else:
+                send_data["response"] = 200
+            self.s.sendto(json.dumps(send_data).encode("utf-8"), client)
+
 
 
 if __name__ == "__main__":
