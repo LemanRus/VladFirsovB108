725a6f2c090dda27e95ba709b694c6d793d5270f
LemanRus <wlad163163@gmail.com>
2022-12-21 08:36:46 +0400

    Hw3_5 Add login action

--- Module3/bestshopever/bestshopever/urls.py
+++ Module3/bestshopever/bestshopever/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('', include('ads.urls')),
+    path('ads/', include('ads.urls')),
 ]
 
 if settings.DEBUG:
