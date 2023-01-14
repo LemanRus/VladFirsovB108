8a07251618d62b3c920135a15aea309b043ce931
LemanRus <wlad163163@gmail.com>
2023-01-15 01:38:39 +0400

    Hw3_8 Start to customize admin site

--- Module3/bestshopever/ads/admin.py
+++ Module3/bestshopever/ads/admin.py
@@ -1,7 +1,41 @@
 from django.contrib import admin
 
-from .models import Advertisement, Category
+from .models import Advertisement, Category, Comment
 
-admin.site.register(Advertisement)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'text', 'date_pub']
+    readonly_fields = ['date_pub']
+    extra = 0
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class AdvertisementInline(admin.TabularInline):
+    model = Advertisement
+    fieldsets = [
+        ('Advertisement content', {'fields': ['title', 'description', 'image', 'category', ]}),
+    ]
+    extra = 0
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    fieldsets =[
+        (None, {'fields': ['author', ]}),
+        ('Advertisement content', {'fields': ['title', 'description', 'image', 'category',]}),
+        ('Date information', {'fields': ['date_pub', 'date_edit']}),
+    ]
+    readonly_fields = ['date_pub', 'date_edit']
+    inlines = [CommentInline]
+    list_display = ['__str__', 'description', 'category', 'date_pub']
+    list_editable = ['category']
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
 
 admin.site.register(Category)
--- Module3/bestshopever/core/admin.py
+++ Module3/bestshopever/core/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser, Rating
+from ads.admin import AdvertisementInline
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [AdvertisementInline, ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Rating)
